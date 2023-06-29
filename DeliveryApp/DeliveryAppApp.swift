323d9c14abe4925d478897f35b51ec45cad2b333
Roman Zamolotov <rzamolotov@gmail.com>
2023-06-29 07:24:59 +0300

    Initial Commit

---
+++ DeliveryApp/DeliveryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliveryAppApp.swift
+//  DeliveryApp
+//
+//  Created by Роман Замолотов on 29.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct DeliveryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
